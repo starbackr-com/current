d7eeb6560b2a09f15a60488a28a3ed8c8e5149c5
Egge <80262989+Egge7@users.noreply.github.com>
2023-02-14 16:31:59 +0100

    tiny fix

--- features/homefeed/components/GetStartedItems.js
+++ features/homefeed/components/GetStartedItems.js
@@ -59,7 +59,7 @@ const GetStartedItems = () => {
             function: async () => {
                 navigation.navigate('PostView', {
                     screen: 'PostModal',
-                    params: {prefilledContent: 'just setting up my nostr account on Current, shoot me a Zap!'},
+                    params: {prefilledContent: 'just setting up my nostr account on Current App, shoot me a Zap!'},
                   });
                 dispatch(setGetStartedItems(1));
                 const getStartedItemsShown = await getData('getStartedItemsShown')
