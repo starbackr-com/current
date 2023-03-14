776b7f0bc394911361fbfb388a8b61a3b4e7c51e
Egge <80262989+Egge7@users.noreply.github.com>
2023-03-14 14:42:57 +0100

    fixes + began homefeed v3

--- features/homefeed/hooks/usePaginatedFeed.js
+++ features/homefeed/hooks/usePaginatedFeed.js
@@ -4,25 +4,83 @@ import { connectedRelayPool, Note, pool } from "../../../utils/nostrV2";
 
 export const usePaginatedFeed = (unixNow) => {
     const [events, setEvents] = useState([]);
-    const [globalUntil, setGlobalUntil] = useState(Math.floor(Date.now() / 1000));
+    const [globalUntil, setGlobalUntil] = useState(
+        Math.floor(Date.now() / 1000)
+    );
     const followedPubkeys = useSelector((state) => state.user.followedPubkeys);
     let timer;
 
     let windowHours = 1;
 
     if (followedPubkeys < 100) {
-        windowHours = 12
+        windowHours = 12;
     } else if (followedPubkeys < 250) {
-        windowHours = 3
+        windowHours = 3;
     }
 
+    const refresh = () => {
+        setGlobalUntil(Math.floor(Date.now() / 1000));
+        get25RootPosts();
+    };
+
+    // const get25RootPostsNew = async () => {
+    //     const now = Math.floor(Date.now() / 1000);
+    //     const relays = connectedRelayPool.map((relay) => {
+    //         return Object.assign(relay, { lastItem: now });
+    //     });
+    //     const rootPosts = new Set();
+    //     await Promise.allSettled(
+    //         relays.map((relay) => {
+    //             return new Promise(async (resolve) => {
+    //                 while ([...rootPosts].length < 25) {
+    //                     await new Promise((resolve) => {
+    //                         const sub = relay.sub(
+    //                             [
+    //                                 {
+    //                                     authors: followedPubkeys,
+    //                                     kinds: [1],
+    //                                     limit: 25,
+    //                                     until: relay.lastItem,
+    //                                 },
+    //                             ],
+    //                             { skipVerification: true }
+    //                         );
+    //                         sub.on("event", (event) => {
+    //                             if (event.created_at > relay.lastItem) {
+    //                                 relay.lastItem = event.created_at;
+    //                             }
+    //                             if (
+    //                                 !event.tags.some((tag) => tag.includes("e"))
+    //                             ) {
+    //                                 const newEvent = new Note(event).save();
+    //                                 rootPosts.add(newEvent);
+    //                                 console.log(rootPosts.size);
+    //                             }
+    //                         });
+    //                         sub.on("eose", () => {
+    //                             resolve();
+    //                         });
+    //                     });
+    //                 }
+    //                 const deduped = new Set([...rootPosts, ...events]);
+    //                 setEvents(
+    //                     [...deduped].sort((a, b) => b.created_at - a.created_at)
+    //                 );
+    //                 resolve();
+    //             });
+    //         })
+    //     );
+    // };
+
     const get25RootPosts = async () => {
-        let until = globalUntil
+        if (followedPubkeys.length < 1) {
+            return;
+        }
+        let until = globalUntil;
         let since = until - 3600;
         const urls = connectedRelayPool.map((relay) => relay.url);
         const results = [];
         while (results.length < 25) {
-            console.log(until)
             await new Promise((resolve) => {
                 const next = () => {
                     const set = new Set([...events, ...results]);
@@ -32,14 +90,18 @@ export const usePaginatedFeed = (unixNow) => {
                     resolve();
                     sub.unsub();
                 };
-                const sub = pool.sub(urls, [
-                    {
-                        authors: followedPubkeys,
-                        kinds: [1],
-                        until,
-                        since,
-                    },
-                ]);
+                const sub = pool.sub(
+                    urls,
+                    [
+                        {
+                            authors: followedPubkeys,
+                            kinds: [1],
+                            until,
+                            since,
+                        },
+                    ],
+                    { skipVerification: true }
+                );
                 timer = setTimeout(next, 3200);
                 sub.on("event", (event) => {
                     clearTimeout(timer);
@@ -50,15 +112,15 @@ export const usePaginatedFeed = (unixNow) => {
                     timer = setTimeout(next, 3000);
                 });
             });
-            until = until - 3600
+            until = until - 3600;
             since = since - 3600;
         }
-        setGlobalUntil(until)
+        setGlobalUntil(until);
     };
 
     useEffect(() => {
         get25RootPosts();
     }, []);
 
-    return [get25RootPosts, events];
+    return [get25RootPosts, events, refresh];
 };
