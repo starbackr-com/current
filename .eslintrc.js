680f3103b70e510dbda4620c942d5bfe5239cb49
Egge <80262989+Egge7@users.noreply.github.com>
2023-04-04 23:09:24 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 0,
+    'react/function-component-definition': [
+      1,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'object-curly-newline': 0,
+  },
+};
