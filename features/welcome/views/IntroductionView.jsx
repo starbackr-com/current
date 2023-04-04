195f4388805648a54aa59bbfba61a7f4a8245309
Egge <80262989+Egge7@users.noreply.github.com>
2023-04-04 23:09:45 +0200

    fixed issues

--- features/welcome/views/IntroductionView.jsx
+++ features/welcome/views/IntroductionView.jsx
@@ -1,71 +1,66 @@
-import { View, Text } from "react-native";
-import React, { useState } from "react";
-import globalStyles from "../../../styles/globalStyles";
-import { IntroductionItem } from "../components";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import CustomButton from "../../../components/CustomButton";
-import { ScrollView } from "react-native-gesture-handler";
-import { generateRandomString } from "../../../utils/cache/asyncStorage";
-import { generateSeedphrase, mnemonicToSeed } from "../../../utils/keys";
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
+import globalStyles from '../../../styles/globalStyles';
+import { IntroductionItem } from '../components';
+import CustomButton from '../../../components/CustomButton';
+import { generateRandomString } from '../../../utils/cache/asyncStorage';
+import { generateSeedphrase, mnemonicToSeed } from '../../../utils/keys';
 
-const IntroductionView = ({ navigation }) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const insets = useSafeAreaInsets();
+function IntroductionView({ navigation }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const insets = useSafeAreaInsets();
 
-    const createHandler = () => {
-        setIsLoading(true);
-        generateRandomString(12);
-        const mem = generateSeedphrase();
-        const sk = mnemonicToSeed(mem);
-        navigation.navigate("EULA", { mem, sk });
-        setIsLoading(false);
-    };
-    return (
-        <View
-            style={[
-                globalStyles.screenContainer,
-                { paddingTop: insets.top, paddingBottom: insets.bottom },
-            ]}
-        >
-            <ScrollView
-                style={{ marginTop: 32, width: "100%" }}
-                horizontal={false}
-            >
-                <View style={{width: '100%', justifyContent: 'center'}}>
-                    <Text
-                        style={[globalStyles.textH2, { textAlign: "center" }]}
-                    >
-                        The power of nostr + bitcoin at your hands!
-                    </Text>
-                </View>
-                <IntroductionItem
-                    title="Runs on nostr"
-                    text="Current is your gateway to the decentralised social network 'nostr'"
-                    icon="globe"
-                />
-                <IntroductionItem
-                    title="You are in control"
-                    text="Everything inside Current is controlled by cryptographic keys that only you have access to."
-                    icon="lock-closed"
-                />
-                <IntroductionItem
-                    title="Powered by Lightning"
-                    text="Zap away with an integrated Bitcoin Lightning Wallet"
-                    icon="flash"
-                />
-                <IntroductionItem
-                    title="Usernames"
-                    text="Current comes with a single username for you nostr and Lightning address"
-                    icon="person-circle-outline"
-                />
-            </ScrollView>
-            <CustomButton
-                text="Let's go!"
-                loading={isLoading}
-                buttonConfig={{ onPress: createHandler }}
-            />
+  const createHandler = () => {
+    setIsLoading(true);
+    generateRandomString(12);
+    const mem = generateSeedphrase();
+    const sk = mnemonicToSeed(mem);
+    navigation.navigate('EULA', { mem, sk });
+    setIsLoading(false);
+  };
+  return (
+    <View
+      style={[
+        globalStyles.screenContainer,
+        { paddingTop: insets.top, paddingBottom: insets.bottom },
+      ]}
+    >
+      <ScrollView style={{ marginTop: 32, width: '100%' }} horizontal={false}>
+        <View style={{ width: '100%', justifyContent: 'center' }}>
+          <Text style={[globalStyles.textH2, { textAlign: 'center' }]}>
+            The power of nostr + bitcoin at your hands!
+          </Text>
         </View>
-    );
-};
+        <IntroductionItem
+          title="Runs on nostr"
+          text="Current is your gateway to the decentralised social network 'nostr'"
+          icon="globe"
+        />
+        <IntroductionItem
+          title="You are in control"
+          text="Everything inside Current is controlled by cryptographic keys that only you have access to."
+          icon="lock-closed"
+        />
+        <IntroductionItem
+          title="Powered by Lightning"
+          text="Zap away with an integrated Bitcoin Lightning Wallet"
+          icon="flash"
+        />
+        <IntroductionItem
+          title="Usernames"
+          text="Current comes with a single username for you nostr and Lightning address"
+          icon="person-circle-outline"
+        />
+      </ScrollView>
+      <CustomButton
+        text="Let's go!"
+        loading={isLoading}
+        buttonConfig={{ onPress: createHandler }}
+      />
+    </View>
+  );
+}
 
 export default IntroductionView;
--- features/welcome/views/UsernameView.jsx
+++ features/welcome/views/UsernameView.jsx
@@ -1,143 +1,138 @@
-import { View, Text, Keyboard, ScrollView } from "react-native";
-import React, { useState } from "react";
-import { CustomButton, Input, LoadingSpinner } from "../../../components";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { usernameRegex } from "../../../constants";
+import { View, Text, Keyboard, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { CustomButton, Input, LoadingSpinner } from '../../../components';
+import { usernameRegex } from '../../../constants';
+import globalStyles from '../../../styles/globalStyles';
 
 const UsernameView = ({ navigation, route }) => {
-    const [error, setError] = useState(false);
-    const [username, setUsername] = useState("");
-    const [available, setAvailable] = useState();
-    const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState(false);
+  const [username, setUsername] = useState('');
+  const [available, setAvailable] = useState();
+  const [isFetching, setIsFetching] = useState();
 
-    const insets = useSafeAreaInsets();
+  const insets = useSafeAreaInsets();
 
-    const { sk, mem, isImport, updateData } = route.params;
+  const { sk, mem, isImport, updateData } = route.params;
 
-    const fetchAvailableUsernames = async () => {
-        setError(false);
-        if (!username.match(usernameRegex)) {
-            setError(true);
-            return;
-        }
-        Keyboard.dismiss();
-        setIsFetching(true);
-        const response = await fetch(
-            `${process.env.BASEURL}/checkuser?name=${username}`
-        );
-        const data = await response.json();
-        setAvailable(data.available);
-        setIsFetching(false);
-    };
-
-    const nextHandler = (address) => {
-        if (isImport && updateData === "none") {
-            navigation.navigate("Profile", {
-                privKey,
-                address,
-                publishProfile: false,
-                isImport,
-            });
-            return;
-        }
-        navigation.navigate("Profile", {
-            screen: "EditProfile",
-            params: { sk, address, mem },
-        });
-    };
-
-    return (
-        <View
-            style={[
-                globalStyles.screenContainer,
-                { paddingTop: insets.top + 6, paddingBottom: insets.bottom },
-            ]}
-        >
-            <Text style={[globalStyles.textBodyBold, { textAlign: "center" }]}>
-                Choose your username
-            </Text>
-            {isImport ? (
-                <Text style={globalStyles.textBody}>
-                    Although you imported an existing key, you still need to
-                    choose a username for your Current wallet. Your nostr
-                    profile will not be affected.
-                </Text>
-            ) : (
-                <Text style={globalStyles.textBody}>
-                    Your username can be used to find you on nostr, but also to
-                    receive payments on the Lightning Network.
-                </Text>
-            )}
-            <View></View>
-            <View style={{ width: "100%", alignItems: "center", margin: 32 }}>
-                <View
-                    style={{
-                        width: "60%",
-                        flexDirection: "row",
-                        alignItems: "center",
-                        justifyContent: "center",
-                    }}
-                >
-                    <Input
-                        textInputConfig={{
-                            value: username,
-                            onChangeText: (value) => {
-                                setUsername(value.toLowerCase());
-                            },
-                            autoCapitalize: "none",
-                            autoCorrect: false,
-                            onsubmit: fetchAvailableUsernames,
-                        }}
-                    />
-                    <CustomButton
-                        icon="search"
-                        containerStyles={{ marginLeft: 6 }}
-                        buttonConfig={{ onPress: fetchAvailableUsernames }}
-                    />
-                </View>
-            </View>
-            {isFetching ? <LoadingSpinner size={50} /> : undefined}
-            <ScrollView
-                style={{ width: "100%" }}
-                contentContainerStyle={{ alignItems: "center" }}
-            >
-                {available && available.length > 0 && !isFetching && !error ? (
-                    <Text style={[globalStyles.textBody, { marginBottom: 32 }]}>
-                        Choose one from the list below
-                    </Text>
-                ) : undefined}
-                {available && available.length > 0 && !isFetching && !error
-                    ? available.map((nip05) => (
-                          <CustomButton
-                              key={nip05}
-                              text={nip05}
-                              containerStyles={{
-                                  width: "80%",
-                                  marginBottom: 18,
-                              }}
-                              buttonConfig={{
-                                  onPress: nextHandler.bind(this, nip05),
-                              }}
-                          />
-                      ))
-                    : undefined}
-                {available &&
-                available.length === 0 &&
-                !isFetching &&
-                !error ? (
-                    <Text style={globalStyles.textBody}>
-                        That username is taken...
-                    </Text>
-                ) : undefined}
-                {error ? (
-                    <Text style={globalStyles.textBodyError}>
-                        Usernames must be between 4 and 32 chars and can only
-                        contain a-z, 0-9
-                    </Text>
-                ) : undefined}
-            </ScrollView>
-        </View>
+  const fetchAvailableUsernames = async () => {
+    setError(false);
+    if (!username.match(usernameRegex)) {
+      setError(true);
+      return;
+    }
+    Keyboard.dismiss();
+    setIsFetching(true);
+    const response = await fetch(
+      `${process.env.BASEURL}/checkuser?name=${username}`,
     );
+    const data = await response.json();
+    setAvailable(data.available);
+    setIsFetching(false);
+  };
+
+  const nextHandler = (address) => {
+    if (isImport && updateData === 'none') {
+      navigation.navigate('Profile', {
+        sk,
+        address,
+        publishProfile: false,
+        isImport,
+      });
+      return;
+    }
+    navigation.navigate('Profile', {
+      screen: 'EditProfile',
+      params: { sk, address, mem },
+    });
+  };
+
+  return (
+    <View
+      style={[
+        globalStyles.screenContainer,
+        { paddingTop: insets.top + 6, paddingBottom: insets.bottom },
+      ]}
+    >
+      <Text style={[globalStyles.textBodyBold, { textAlign: 'center' }]}>
+        Choose your username
+      </Text>
+      {isImport ? (
+        <Text style={globalStyles.textBody}>
+          Although you imported an existing key, you still need to choose a
+          username for your Current wallet. Your nostr profile will not be
+          affected.
+        </Text>
+      ) : (
+        <Text style={globalStyles.textBody}>
+          Your username can be used to find you on nostr, but also to receive
+          payments on the Lightning Network.
+        </Text>
+      )}
+      <View style={{ width: '100%', alignItems: 'center', margin: 32 }}>
+        <View
+          style={{
+            width: '60%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Input
+            textInputConfig={{
+              value: username,
+              onChangeText: (value) => {
+                setUsername(value.toLowerCase());
+              },
+              autoCapitalize: 'none',
+              autoCorrect: false,
+              onsubmit: fetchAvailableUsernames,
+            }}
+          />
+          <CustomButton
+            icon="search"
+            containerStyles={{ marginLeft: 6 }}
+            buttonConfig={{ onPress: fetchAvailableUsernames }}
+          />
+        </View>
+      </View>
+      {isFetching ? <LoadingSpinner size={50} /> : undefined}
+      <ScrollView
+        style={{ width: '100%' }}
+        contentContainerStyle={{ alignItems: 'center' }}
+      >
+        {available && available.length > 0 && !isFetching && !error ? (
+          <Text style={[globalStyles.textBody, { marginBottom: 32 }]}>
+            Choose one from the list below
+          </Text>
+        ) : undefined}
+        {available && available.length > 0 && !isFetching && !error
+          ? available.map((nip05) => (
+            <CustomButton
+              key={nip05}
+              text={nip05}
+              containerStyles={{
+                width: '80%',
+                marginBottom: 18,
+              }}
+              buttonConfig={{
+                onPress: nextHandler.bind(this, nip05),
+              }}
+            />
+          ))
+          : undefined}
+        {available && available.length === 0 && !isFetching && !error ? (
+          <Text style={globalStyles.textBody}>That username is taken...</Text>
+        ) : undefined}
+        {error ? (
+          <Text style={globalStyles.textBodyError}>
+            Usernames must be between 4 and 32 chars and can only contain a-z,
+            0-9
+          </Text>
+        ) : undefined}
+      </ScrollView>
+    </View>
+  );
 };
 
 export default UsernameView;
