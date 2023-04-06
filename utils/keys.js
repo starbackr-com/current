2a7913d41a984a2b10ee421dbeaf8cd3589f1ec2
Egge <80262989+Egge7@users.noreply.github.com>
2023-04-06 23:53:31 +0200

    readability

--- utils/keys.js
+++ utils/keys.js
@@ -1,13 +1,14 @@
-import { nip06 } from "nostr-tools";
-import { generateMnemonic } from "@scure/bip39";
-import { wordlist } from "@scure/bip39/wordlists/english.js";
+/* eslint-disable import/no-extraneous-dependencies */
+import { nip06 } from 'nostr-tools';
+import { generateMnemonic } from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english';
 
 export const generateSeedphrase = () => {
-    const mem = generateMnemonic(wordlist, 128).split(' ');
-    return mem;
+  const mem = generateMnemonic(wordlist, 128).split(' ');
+  return mem;
 };
 
 export const mnemonicToSeed = (words) => {
-    const privKey = nip06.privateKeyFromSeedWords(words.join(" "));
-    return privKey;
+  const privKey = nip06.privateKeyFromSeedWords(words.join(' '));
+  return privKey;
 };
--- utils/nostr/keys.js
+++ utils/nostr/keys.js
@@ -1,58 +1,56 @@
-import { bech32 } from "bech32";
-
-const utf8Encoder = new TextDecoder();
+import { bech32 } from 'bech32';
 
 function buf2hex(buffer) {
-    return [...new Uint8Array(buffer)]
-        .map((x) => x.toString(16).padStart(2, "0"))
-        .join("");
+  return [...new Uint8Array(buffer)]
+    .map((x) => x.toString(16).padStart(2, '0'))
+    .join('');
 }
 
 function toByteArray(hexString) {
-    var result = [];
-    for (var i = 0; i < hexString.length; i += 2) {
-        result.push(parseInt(hexString.substr(i, 2), 16));
-    }
-    return result;
+  const result = [];
+  for (const i = 0; i < hexString.length; i += 2) {
+    result.push(parseInt(hexString.substr(i, 2), 16));
+  }
+  return result;
 }
 
 export const decodePubkey = (pubkey) => {
-    try {
-        let { prefix: hrp, words: dataPart } = bech32.decode(pubkey, 2000);
-        let requestByteArray = bech32.fromWords(dataPart);
-        let decoded = buf2hex(requestByteArray);
-        return decoded;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    let { prefix: hrp, words: dataPart } = bech32.decode(pubkey, 2000);
+    let requestByteArray = bech32.fromWords(dataPart);
+    let decoded = buf2hex(requestByteArray);
+    return decoded;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const encodePubkey = (pubkeyInHex) => {
-    try {
-        let words = bech32.toWords(toByteArray(pubkeyInHex))
-        const encoded = bech32.encode('npub', words)
-        return encoded;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const words = bech32.toWords(toByteArray(pubkeyInHex));
+    const encoded = bech32.encode('npub', words);
+    return encoded;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const encodeSeckey = (skInHex) => {
-    try {
-        let words = bech32.toWords(toByteArray(skInHex))
-        const encoded = bech32.encode('nsec', words)
-        return encoded;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const words = bech32.toWords(toByteArray(skInHex));
+    const encoded = bech32.encode('nsec', words);
+    return encoded;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const encodeNoteID = (noteIdinHey) => {
-    try {
-        let words = bech32.toWords(toByteArray(noteIdinHey))
-        const encoded = bech32.encode('note', words)
-        return encoded;
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    let words = bech32.toWords(toByteArray(noteIdinHey));
+    const encoded = bech32.encode('note', words);
+    return encoded;
+  } catch (err) {
+    console.log(err);
+  }
+};
