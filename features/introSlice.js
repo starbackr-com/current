4286797f81ba267720a4f0f553eec7d1e8a3b8a1
Egge <80262989+Egge7@users.noreply.github.com>
2023-04-06 07:20:58 +0200

    updated redux slices for eslint

--- features/introSlice.js
+++ features/introSlice.js
@@ -1,28 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    twitterModalShown: false,
-    getStartedItems: [0,1,2,3]
+  twitterModalShown: false,
+  getStartedItems: [0, 1, 2, 3],
 };
 
 export const introSlice = createSlice({
-    name: "intro",
-    initialState,
-    reducers: {
-        setTwitterModal: (state) => {
-            state.twitterModalShown = true;
-        },
-        setGetStartedItems: (state, action) => {
-            const newArray = state.getStartedItems.filter(item => item !== action.payload)
-            state.getStartedItems = newArray
-        },
-        resetAll: (state) => {
-            state.twitterModalShown = false
-            state.getStartedItems = [0,1,2,3]
-        }
+  name: 'intro',
+  initialState,
+  reducers: {
+    setTwitterModal: (state) => {
+      state.twitterModalShown = true;
     },
+    setGetStartedItems: (state, action) => {
+      const newArray = state.getStartedItems.filter(
+        (item) => item !== action.payload,
+      );
+      state.getStartedItems = newArray;
+    },
+    resetAll: (state) => {
+      state.twitterModalShown = false;
+      state.getStartedItems = [0, 1, 2, 3];
+    },
+  },
 });
 
-export const { setTwitterModal, resetAll, setGetStartedItems } = introSlice.actions;
+export const { setTwitterModal, resetAll, setGetStartedItems } =
+  introSlice.actions;
 
 export default introSlice.reducer;
--- features/messagesSlice.js
+++ features/messagesSlice.js
@@ -1,56 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    messages: [],
-    noteIds: [],
-    users: {},
-    relayReady: false,
+  messages: [],
+  noteIds: [],
+  users: {},
+  relayReady: false,
 };
 
 export const messageSlice = createSlice({
-    name: "messages",
-    initialState,
-    reducers: {
-        addMessage: (state, action) => {
-            const newMessage = action.payload;
-            const exists = state.messages.find(
-                (message) => message.id === newMessage.id
-            );
-            if (exists) {
-                return;
-            }
-            const updatedArray = [...state.messages, action.payload].sort((a, b) => b.created_at - a.created_at);
-            state.messages = updatedArray;
-        },
-        removeAuthorsMessages: (state, action) => {
-            const author = action.payload;
-            state.messages = state.messages.filter(
-                (message) => message.pubkey !== author
-            );
-        },
-        addUser: (state, action) => {
-            const newUser = action.payload.user;
-            const exists = state.users.hasOwnProperty(newUser.pubkey);
-            if (exists) {
-                state.users[newUser.pubkey] =
-                    state.users[newUser.pubkey].created_at < newUser.created_at
-                        ? newUser
-                        : state.users[newUser.pubkey];
-            } else state.users[newUser.pubkey] = newUser;
-        },
-        hydrate: (state, action) => {
-            const databaseUsers = action.payload
-            state.users = databaseUsers
-        },
-        clearStore: (state) => {
-            state.messages = [];
-            state.users = {};
-            state.relayReady = false;
-        },
+  name: 'messages',
+  initialState,
+  reducers: {
+    addMessage: (state, action) => {
+      const newMessage = action.payload;
+      const exists = state.messages.find(
+        (message) => message.id === newMessage.id,
+      );
+      if (exists) {
+        return;
+      }
+      const updatedArray = [...state.messages, action.payload].sort(
+        (a, b) => b.created_at - a.created_at,
+      );
+      state.messages = updatedArray;
     },
+    removeAuthorsMessages: (state, action) => {
+      const author = action.payload;
+      state.messages = state.messages.filter(
+        (message) => message.pubkey !== author,
+      );
+    },
+    addUser: (state, action) => {
+      const newUser = action.payload.user;
+      const exists = state.users.hasOwnProperty(newUser.pubkey);
+      if (exists) {
+        state.users[newUser.pubkey] =
+          state.users[newUser.pubkey].created_at < newUser.created_at
+            ? newUser
+            : state.users[newUser.pubkey];
+      } else state.users[newUser.pubkey] = newUser;
+    },
+    hydrate: (state, action) => {
+      const databaseUsers = action.payload;
+      state.users = databaseUsers;
+    },
+    clearStore: (state) => {
+      state.messages = [];
+      state.users = {};
+      state.relayReady = false;
+    },
+  },
 });
 
-export const { addMessage, addUser, removeAuthorsMessages, clearStore, hydrate } =
-    messageSlice.actions;
+export const {
+  addMessage,
+  addUser,
+  removeAuthorsMessages,
+  clearStore,
+  hydrate,
+} = messageSlice.actions;
 
 export default messageSlice.reducer;
