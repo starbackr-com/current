9e005c56c4f4f06d15a3c5aa6f03efeb0f57d052
Egge <80262989+Egge7@users.noreply.github.com>
2023-04-12 13:20:04 +0200

    refactoring

--- features/relays/components/RelaySettingsHeader.jsx
+++ features/relays/components/RelaySettingsHeader.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { useNavigation } from '@react-navigation/native';
 import BackButton from '../../../components/BackButton';
 import { colors } from '../../../styles';
 
--- features/relays/nav/RelaySettingsNav.jsx
+++ features/relays/nav/RelaySettingsNav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AddRelayView, RelaysOverviewView } from '../views';
@@ -6,7 +7,13 @@ import RelaySettingsHeader from '../components/RelaySettingsHeader';
 const Stack = createStackNavigator();
 
 const RelaySettingsNav = () => (
-  <Stack.Navigator screenOptions={{ header: (props) => <RelaySettingsHeader {...props}/> }}>
+  <Stack.Navigator
+    screenOptions={{
+      header: ({ route, navigation }) => (
+        <RelaySettingsHeader route={route} navigation={navigation} />
+      ),
+    }}
+  >
     <Stack.Screen name="Overview" component={RelaysOverviewView} />
     <Stack.Screen name="Add" component={AddRelayView} />
   </Stack.Navigator>
