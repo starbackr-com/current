1d689651ce0feafe29a5c3846a51bad597f54c85
Egge <80262989+Egge7@users.noreply.github.com>
2023-04-14 11:30:44 +0200

    added styling and normalizing to add relay

--- features/relays/nav/RelaySettingsNav.jsx
+++ features/relays/nav/RelaySettingsNav.jsx
@@ -1,22 +1,30 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useHeaderHeight } from '@react-navigation/elements';
 import { AddRelayView, RelaysOverviewView } from '../views';
 import RelaySettingsHeader from '../components/RelaySettingsHeader';
 
 const Stack = createStackNavigator();
 
-const RelaySettingsNav = () => (
-  <Stack.Navigator
-    screenOptions={{
-      header: ({ route, navigation }) => (
-        <RelaySettingsHeader route={route} navigation={navigation} />
-      ),
-    }}
-  >
-    <Stack.Screen name="Overview" component={RelaysOverviewView} />
-    <Stack.Screen name="Add" component={AddRelayView} />
-  </Stack.Navigator>
-);
+const RelaySettingsNav = () => {
+  const headerHeight = useHeaderHeight();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        header: ({ route, navigation }) => (
+          <RelaySettingsHeader route={route} navigation={navigation} />
+        ),
+      }}
+    >
+      <Stack.Screen name="Overview" component={RelaysOverviewView} />
+      <Stack.Screen
+        name="Add"
+        component={AddRelayView}
+        initialParams={{ headerHeight }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default RelaySettingsNav;
--- features/relays/views/AddRelayView.jsx
+++ features/relays/views/AddRelayView.jsx
@@ -1,4 +1,4 @@
-import { Alert, View } from 'react-native';
+import { Alert, KeyboardAvoidingView, Platform, View } from 'react-native';
 import React, { useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useDispatch } from 'react-redux';
@@ -6,34 +6,74 @@ import { globalStyles } from '../../../styles';
 import { CustomButton, Input } from '../../../components';
 import { pool } from '../../../utils/nostrV2';
 import { addRelay } from '../relaysSlice';
+import { useHeaderHeight } from '@react-navigation/elements';
 
-const AddRelayView = () => {
+const AddRelayView = ({ navigation, route }) => {
   const [urlInput, setUrlInput] = useState();
+  const [canAdd, setCanAdd] = useState(false);
   const dispatch = useDispatch();
-  const submitHandler = async () => {
+  const { headerHeight } = route.params;
+  const localHeaderHeight = useHeaderHeight();
+
+  const connectHandler = async () => {
     try {
       const relay = await pool.ensureRelay(urlInput);
       if (!relay) {
         throw new Error('Could not connect to relay!');
       }
-      dispatch(addRelay([{ url: relay.url, read: true, write: true }]));
+      setCanAdd(true);
     } catch (e) {
-      Alert.alert('Something went wrong... Could not add relay.');
+      Alert.alert('Something went wrong... Could not connect to relay.');
+    }
+  };
+
+  const addHandler = () => {
+    if (canAdd) {
+      const normalizedUrl = new URL(urlInput);
+      const relayObj = { url: normalizedUrl.toString(), read: true, write: true };
+      console.log(relayObj);
+      dispatch(addRelay([relayObj]));
+      navigation.goBack();
     }
   };
   return (
-    <View style={[globalStyles.screenContainer, { paddingTop: 6 }]}>
-      <ScrollView style={{ width: '100%' }}>
-        <Input
-          label="Relay URL"
-          textInputConfig={{ onChangeText: setUrlInput, autoCapitalize: 'none', autoComplete: 'off', autoCorrect: false }}
-        />
-        <CustomButton
-          text="Add Relay"
-          buttonConfig={{ onPress: submitHandler }}
-        />
+    <KeyboardAvoidingView
+      style={[globalStyles.screenContainer, { paddingTop: 6 }]}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={headerHeight + localHeaderHeight}
+    >
+      <ScrollView
+        style={{ width: '100%' }}
+        contentContainerStyle={{
+          justifyContent: 'space-between',
+          height: '100%',
+        }}
+      >
+        <View>
+          <Input
+            label="Relay URL"
+            textInputConfig={{
+              onChangeText: setUrlInput,
+              autoCapitalize: 'none',
+              autoComplete: 'off',
+              autoCorrect: false,
+            }}
+          />
+        </View>
+        <View style={{ paddingBottom: 12 }}>
+          <CustomButton
+            text="Test Connection"
+            buttonConfig={{ onPress: connectHandler }}
+            containerStyles={{ marginBottom: 12 }}
+          />
+          <CustomButton
+            text="Add Relay"
+            buttonConfig={{ onPress: addHandler }}
+            disabled={!canAdd}
+          />
+        </View>
       </ScrollView>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
