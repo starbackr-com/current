5c28caffad802aec945e77378b1c7ebf75e17612
Egge <80262989+Egge7@users.noreply.github.com>
2023-04-19 16:07:46 +0200

    added safearea to profile header

--- features/profile/components/ProfileHeader.js
+++ features/profile/components/ProfileHeader.js
@@ -1,12 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React from "react";
 import BackButton from "../../../components/BackButton";
 import { useIsAuthed } from "../hooks/useIsAuthed";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { colors, globalStyles } from "../../../styles";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const ProfileHeader = ({ route, navigation }) => {
     const check = useIsAuthed(route?.params?.pubkey);
+    const insets = useSafeAreaInsets();
     return (
         <View
             style={{
@@ -15,6 +17,7 @@ const ProfileHeader = ({ route, navigation }) => {
                 backgroundColor: colors.backgroundPrimary,
                 padding: 12,
                 alignItems: "center",
+                paddingTop: insets.top
             }}
         >
             <View style={{ width: "25%" }}>
