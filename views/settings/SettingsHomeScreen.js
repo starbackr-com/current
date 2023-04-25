5f97a28feb4a518c87607059c16aa55c2d3cce7b
starbackr-dev <arun@starbackr.com>
2023-04-25 18:04:37 -0400

    push notification v1 working

--- views/settings/SettingsHomeScreen.js
+++ views/settings/SettingsHomeScreen.js
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { View, Text, FlatList, Pressable } from 'react-native';
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteValue } from '../../utils/secureStore';
 import { logOut } from '../../features/authSlice';
@@ -14,6 +14,11 @@ import { removeData } from '../../utils/cache/asyncStorage';
 import appJson from '../../app.json';
 import { generateRandomString } from '../../utils/cache/asyncStorage';
 import { colors, globalStyles } from '../../styles';
+import Checkbox from 'expo-checkbox';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { useSelector } from "react-redux";
+
 
 const settings = [
   'Payment Settings',
@@ -23,7 +28,10 @@ const settings = [
   'Delete Account',
 ];
 
+
+
 const SettingItem = ({ item, onNav }) => {
+
   return (
     <Pressable
       style={({ pressed }) => [
@@ -46,7 +54,32 @@ const SettingItem = ({ item, onNav }) => {
   );
 };
 
+const pushHandler = async () => {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  try {
+
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+  } catch (e) {
+
+    console.log('push error: ', e);
+
+  } finally {
+
+  }
+
+};
+
+
+
+
 const SettingsHomeScreen = ({ navigation }) => {
+
+
+  const { pubKey, walletBearer } = useSelector((state) => state.auth);
+  const [ispushnotifyChecked, setpushnotifyChecked] = useState(false);
   const dispatch = useDispatch();
   const navigationHandler = (route) => {
     navigation.navigate(route);
@@ -62,6 +95,44 @@ const SettingsHomeScreen = ({ navigation }) => {
     dispatch(logOut());
     dispatch(resetAll());
   };
+
+
+
+  const pushtoken = async () => {
+
+    const settings = await Notifications.getPermissionsAsync();
+
+    console.log(settings);
+
+    if (settings.granted) {
+          alert('Push notification is enabled..!');
+          //return;
+
+    }
+
+    const token = await registerForPushNotificationsAsync();
+
+    if (token) {
+
+      const response = await fetch(`${process.env.BASEURL}/v2/pushtoken`, {
+          method: "POST",
+          body: JSON.stringify({token : token}),
+          headers: {
+              "content-type": "application/json",
+              Authorization: `Bearer ${walletBearer}`,
+          },
+      });
+
+      const data = await response.json();
+      console.log(data);
+      if (data.data != 'updated') alert('Push token update failed. Please contact support');
+
+    }
+
+
+
+
+  }
 
   const introHandler = async () => {
     await AsyncStorage.removeItem('appId');
@@ -86,6 +157,30 @@ const SettingsHomeScreen = ({ navigation }) => {
           buttonConfig={{ onPress: logoutHandler }}
         />
       </View>
+
+      <View
+        style={
+          (globalStyles.screenContainer,
+          {
+            width: '100%',
+            justifyContent: 'space-evenly',
+            flexDirection: 'row',
+            marginBottom: 200,
+          })
+        }
+      >
+
+      <Text
+        style={
+          (globalStyles.textH2, { color: colors.primary500, marginTop: 32 })
+        }
+        onPress={pushtoken}
+      >
+        Enable Push notification?
+      </Text>
+      </View>
+
+
       <Text
         style={[
           globalStyles.textBody,
@@ -103,5 +198,47 @@ const SettingsHomeScreen = ({ navigation }) => {
     </View>
   );
 };
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  console.log(Device);
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync({ios: {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+      allowAnnouncements: true,
+    },
+    });
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+
+
+
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 
 export default SettingsHomeScreen;
