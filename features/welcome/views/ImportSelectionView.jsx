13ee32d824ba5fadd3d225e4770bbbaa829100bf
Egge <80262989+Egge7@users.noreply.github.com>
2023-05-30 17:30:58 +0200

    translated ImportSelection

--- features/welcome/views/ImportSelectionView.jsx
+++ features/welcome/views/ImportSelectionView.jsx
@@ -1,12 +1,14 @@
 import { View, Text, ScrollView, Alert } from 'react-native';
 import React from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useTranslation } from 'react-i18next';
 import { globalStyles } from '../../../styles';
 import { ImportTypeItem } from '../components';
 import { CustomButton } from '../../../components';
 
 const ImportSelectionView = ({ navigation }) => {
   const insets = useSafeAreaInsets();
+  const { t } = useTranslation(['welcome', 'common']);
   return (
     <View
       style={[
@@ -14,16 +16,14 @@ const ImportSelectionView = ({ navigation }) => {
         { paddingTop: insets.top + 12, paddingBottom: insets.bottom },
       ]}
     >
-      <Text style={globalStyles.textH2}>
-        Choose the type of key that you want to import...
-      </Text>
+      <Text style={globalStyles.textH2}>{t('ImportSelectionView_H2')}</Text>
       <ScrollView
         style={{ width: '100%', flex: 1 }}
         contentContainerStyle={{ alignItems: 'center' }}
       >
         <ImportTypeItem
-          title="Single Key"
-          text="Import a single private key"
+          title={t('ImportSelectionView_H3_Item_1')}
+          text={t('ImportSelectionView_Body_Item_1')}
           example="nsec1wae9gwn52p2qa4f50xhe2hrefqn8kat4..."
           icon="key"
           onPress={() => {
@@ -31,8 +31,8 @@ const ImportSelectionView = ({ navigation }) => {
           }}
         />
         <ImportTypeItem
-          title="Seed Words"
-          text="Derive keys from a Seed Phrase"
+          title={t('ImportSelectionView_H3_Item_2')}
+          text={t('ImportSelectionView_Body_Item_2')}
           example="target traffic extend boss maximum zero illegal much people bitter crack fire"
           icon="newspaper"
           onPress={() => {
@@ -40,21 +40,25 @@ const ImportSelectionView = ({ navigation }) => {
           }}
         />
         <ImportTypeItem
-          title="I don't have keys"
-          text="You do not have any nostr-keys yet? No problem!"
+          title={t('ImportSelectionView_H3_Item_3')}
+          text={t('ImportSelectionView_Body_Item_3')}
           icon="help"
           onPress={() => {
-            Alert.alert('No Keys?', 'Do you want to create a new key-pair?', [
-              {
-                text: 'Yes',
-                onPress: () => {
-                  navigation.navigate('Username');
+            Alert.alert(
+              t('ImportSelectionView_H2_Item_3_Action'),
+              t('ImportSelectionView_Body_Item_3_Action'),
+              [
+                {
+                  text: t('Common_Yes'),
+                  onPress: () => {
+                    navigation.navigate('Username');
+                  },
                 },
-              },
-              {
-                text: 'No',
-              },
-            ]);
+                {
+                  text: t('Common_No'),
+                },
+              ],
+            );
           }}
         />
       </ScrollView>
